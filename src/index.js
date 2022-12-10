17d4bc9115531917c92db2c9c8f84c2885d4eb82
afuscript <hfy8358@126.com>
2022-12-10 22:05:38 +0800

    chore:soga初始化

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      Redux-Soga
+    </div>
+  </React.StrictMode>
+);
